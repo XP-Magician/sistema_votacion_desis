dd04fa7e4969d50b39d3a3af26d84767e2cc264b
Diego <diegofabrega18@gmail.com>
2024-04-09 17:42:07 -0400

    FIXES: Agregado parametro para recuperar votos y devolucion directa de formato json

--- requests/voto.php
+++ requests/voto.php
@@ -13,16 +13,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         "id_medio_informacion" => $_POST["medio_comunicacion"][0]
     ];
     $resultado = $controller_voto->addVoto($voto_info);
-    echo json_encode(["resultado" => $resultado]);
+    echo json_encode($resultado);
 }
 
 //GET
 else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $controller_voto = new VotoController();
-    $resultado;
-    if (isset($_GET["alias"])) $resultado = $controller_voto->getByAlias($_GET["alias"]);
-    elseif (isset($_GET["rut"])) $resultado = $controller_voto->getByRut($_GET["rut"]);
-    elseif (isset($_GET["correo"])) $resultado = $controller_voto->getByCorreo($_GET["correo"]);
-    else $resultado = $controller_voto->getVotos();
-    echo json_encode($resultado);
+    if (isset($_GET["alias"])) echo json_encode($controller_voto->getByAlias($_GET["alias"]));
+    elseif (isset($_GET["rut"])) echo json_encode($controller_voto->getByRut($_GET["rut"]));
+    elseif (isset($_GET["correo"])) echo json_encode($controller_voto->getByCorreo($_GET["correo"]));
+    elseif (isset($_GET["full"])) echo json_encode($controller_voto->getVotos());
 }
