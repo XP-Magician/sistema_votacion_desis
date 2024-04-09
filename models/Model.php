089590b2d4e330f3022669d7b0aab01d1fa27cb9
Diego <diegofabrega18@gmail.com>
2024-04-09 02:21:56 -0400

    UPDATE: Agregada conexion a la base de datos y metodos heredables

---
+++ models/Model.php
@@ -0,0 +1,67 @@
+<?php
+// Requerir constantes de configuracion
+require_once dirname(__DIR__) . '/config.php';
+class Model
+{
+    protected $conexion;
+    protected $tabla;
+    protected $campo_pk;
+
+    private function conectarDb()
+    {
+        if ($this->conexion == null) {
+            try {
+                $this->conexion = new PDO("mysql:host=" . HOST . ";charset=utf8;dbname=" . DB, DB_USER, PASSWORD);
+                $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $exception) {
+                die($exception->getMessage());
+            }
+        }
+    }
+
+    function __construct()
+    {
+        $this->conectarDb();
+    }
+
+    function getRegistros()
+    {
+        try {
+            $query =  $this->conexion->prepare('SELECT * FROM ' . $this->tabla);
+            $query->execute();
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    function getRegistro($valor_pk)
+    {
+        try {
+            $query =  $this->conexion->prepare('SELECT * FROM ' . $this->tabla . ' WHERE ' . $this->campo_pk . ' = :pk');
+            $query->bindValue('pk', $valor_pk);
+            $query->execute();
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    function existeRegistro($valor_pk)
+    {
+        try {
+            $query =  $this->conexion->prepare('SELECT * FROM ' . $this->tabla . ' WHERE ' . $this->campo_pk . ' = :pk');
+            $query->bindValue('pk', $valor_pk);
+            $query->execute();
+            if ($query->rowCount() > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+}
