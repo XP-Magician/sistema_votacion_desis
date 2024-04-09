b9489e65b81421c5e8c83383c160fe9afa57e33e
Diego <diegofabrega18@gmail.com>
2024-04-09 14:43:44 -0400

    UPDATE: Agregado metodo para añadir votos

--- models/VotoModel.php
+++ models/VotoModel.php
@@ -16,4 +16,24 @@ class VotoModel extends Model
     {
         $this->campo_pk = $nombre;
     }
+
+    function addVoto($array_asoc)
+    {
+        try {
+            $query = $this->conexion->prepare('INSERT INTO ' . $this->tabla . ' 
+            (rut_voto, nombre_votante, alias, correo, id_comuna, id_candidato, id_medio_informacion) 
+            VALUES (:r,:n,:al,:cor,:idc,:idca,:idm)');
+            $query->bindValue(":r", $array_asoc["rut_voto"]);
+            $query->bindValue(":n", $array_asoc["nombre_votante"]);
+            $query->bindValue(":al", $array_asoc["alias"]);
+            $query->bindValue(":cor", $array_asoc["correo"]);
+            $query->bindValue(":idc", $array_asoc["id_comuna"]);
+            $query->bindValue(":idca", $array_asoc["id_candidato"]);
+            $query->bindValue(":idm", $array_asoc["id_medio_informacion"]);
+            $query->execute();
+            return true;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
 }
