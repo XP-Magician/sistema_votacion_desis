0f863ed3229aa4a1d0e5092456d1f312b99fce56
Diego <diegofabrega18@gmail.com>
2024-04-09 13:16:38 -0400

    ADD: Controlador para el modelo voto

---
+++ controllers/VotoController.php
@@ -0,0 +1,32 @@
+<?php
+require_once dirname(__DIR__) . '/config.php';
+require_once(MODELS_VOTO);
+require_once(CONTROLLERS_BASE);
+
+class VotoController extends Controller
+{
+
+    function __construct()
+    {
+        $model = new VotoModel();
+        parent::__construct($model);
+    }
+
+    function getByRut($rut)
+    {
+        $this->model->setCampoBusqueda('rut_voto');
+        return $this->model->getRegistro($rut);
+    }
+
+    function getByCorreo($correo)
+    {
+        $this->model->setCampoBusqueda('correo');
+        return $this->model->getRegistro($correo);
+    }
+
+    function getByAlias($alias)
+    {
+        $this->model->setCampoBusqueda('alias');
+        return $this->model->getRegistro($alias);
+    }
+}
