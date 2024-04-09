875b81d2963870be31cc0e0aa6c9ca68a79e2d6a
Diego <diegofabrega18@gmail.com>
2024-04-09 03:17:25 -0400

    UPDATE: Agregado evento para cambio de comunas mostradas en base a la region seleccionada

--- scripts/handleData.js
+++ scripts/handleData.js
@@ -1,25 +1,56 @@
 $(document).ready(function () {
   // === RECUPERAR REGIONES ===
+
   const selectRegion = $("#region");
+  function recuperarRegiones(data) {
+    const dataArray = JSON.parse(data);
+    $.each(dataArray, function (index, item) {
+      selectRegion.append(
+        $("<option>", {
+          value: item.id_region,
+          text: item.nombre_region,
+        })
+      );
+    });
+    // Para mostrar regiones en primera carga
+    selectRegion.trigger("change");
+  }
   $.ajax({
     type: "GET",
     url: "http://localhost/sistema_votacion_desis/requests/region.php",
-    success: function (data) {
-      // Iterar sobre los datos y agregar opciones al select
-      const dataArray = JSON.parse(data);
-      $.each(dataArray, function (index, item) {
-        selectRegion.append(
-          $("<option>", {
-            value: item.id_region,
-            text: item.nombre_region,
-          })
-        );
-      });
-    },
+    success: recuperarRegiones,
     error: function (err) {
       alert("Ocurrio un error");
     },
   });
 
   // === RECUPERAR COMUNAS EN BASE A REGIONES ===
+
+  const selectComuna = $("#comuna");
+  function recuperarComunas() {
+    const idRegion = selectRegion.val();
+    $.ajax({
+      type: "GET",
+      data: { id_region: idRegion },
+      url: "http://localhost/sistema_votacion_desis/requests/comuna.php",
+      success: function (data) {
+        const dataArray = JSON.parse(data);
+        selectComuna.empty();
+        $.each(dataArray, function (index, item) {
+          selectComuna.append(
+            $("<option>", {
+              value: item.id_comuna,
+              text: item.nombre_comuna,
+            })
+          );
+        });
+      },
+      error: function (err) {
+        alert("Ocurrio un error");
+      },
+    });
+  }
+  selectRegion.change(recuperarComunas);
+
+  // === RECUPERAR CANDIDATOS ===
 });
